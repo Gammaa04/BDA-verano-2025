d425e108cf011fedfaa4296609effe9a2f32f21f
Gammaa04 <Jesus.soto248336@potros.itson.edu.mx>
2025-06-12 17:16:55 -0700

    Add files via upload

---
+++ BDA Verano/BDA_verano/src/main/java/com/mycompany/bda_verano/BDA_verano.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bda_verano;
+
+/**
+ *
+ * @author gamae
+ */
+public class BDA_verano {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
