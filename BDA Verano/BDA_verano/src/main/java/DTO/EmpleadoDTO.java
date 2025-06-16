a32fe1f92e2b1f2afecefdf6967a97103fc0ae54
angelgbd <baeltranangel@gmail.com>
2025-06-15 21:23:41 -0700

    cambio en empleadoDTO
    se cambio la sintaxis para Empleado DTO

--- BDA Verano/BDA_verano/src/main/java/DTO/EmpleadoDTO.java
+++ BDA Verano/BDA_verano/src/main/java/DTO/EmpleadoDTO.java
@@ -22,16 +22,40 @@ public class EmpleadoDTO {
         this.tipo = tipo;
     }
 
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
-    public String getNombreCompleto() { return nombreCompleto; }
-    public void setNombreCompleto(String nombreCompleto) { this.nombreCompleto = nombreCompleto; }
-    public EstadoEmpleado getEstado() { return estado; }
-    public void setEstado(EstadoEmpleado estado) { this.estado = estado; }
-    public String getUsuario() { return usuario; }
-    public void setUsuario(String usuario) { this.usuario = usuario; }
-    public int getDepartamentoId() { return departamentoId; }
-    public void setDepartamentoId(int departamentoId) { this.departamentoId = departamentoId; }
-    public TipoEmpleado getTipo() { return tipo; }
-    public void setTipo(TipoEmpleado tipo) { this.tipo = tipo; }
+    public int getId() { 
+        return id; 
+    }
+    public void setId(int id) {
+        this.id = id; 
+    }
+    public String getNombreCompleto() { 
+        return nombreCompleto; 
+    }
+    public void setNombreCompleto(String nombreCompleto) {
+        this.nombreCompleto = nombreCompleto; 
+    }
+    public EstadoEmpleado getEstado() {
+        return estado; 
+    }
+    public void setEstado(EstadoEmpleado estado) {
+        this.estado = estado; 
+    }
+    public String getUsuario() {
+        return usuario; 
+    }
+    public void setUsuario(String usuario) {
+        this.usuario = usuario; 
+    }
+    public int getDepartamentoId() {
+        return departamentoId; 
+    }
+    public void setDepartamentoId(int departamentoId) {
+        this.departamentoId = departamentoId; 
+    }
+    public TipoEmpleado getTipo() {
+        return tipo; 
+    }
+    public void setTipo(TipoEmpleado tipo) {
+        this.tipo = tipo; 
+    }
 }
