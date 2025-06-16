ced437ab8418e394b41570d6970aacac43fa0ba0
Gamm4a <gamael77c11@gmail.com>
2025-06-15 18:33:02 -0700

    Conexion y POM
    implementación de la conexión y del pom y la creación sobre los paquetes

---
+++ BDA Verano/BDA_verano/src/main/java/PRUEBAS/BDA_verano.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package PRUEBAS;
+
+/**
+ *
+ * @author Jesus Gammael Soto Escalante 248336
+ */
+public class BDA_verano {
+    public static void main(String[] args) {
+        System.out.println("hola pendejo");
+        
+        
+    }
+    
+}
---
+++ BDA Verano/BDA_verano/src/main/java/Persistencia/ConexionDB.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Persistencia;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Jesus Gammael Soto Escalante 248336
+ */
+public class ConexionDB implements IConexionBD {
+    private final String SERVER = "127.0.0.1";
+    private final String BASE_DATOS = "crud";
+    private final String CADENA_CONEXION = "jdbc:mysql://" + SERVER + "/" + BASE_DATOS;
+    private final String USUARIO = "root";
+    private final String CONTRASEÑA = "Ensalada";
+
+    @Override
+    public Connection crearConexion() throws SQLException {
+        Connection conexion = DriverManager.getConnection(CADENA_CONEXION, USUARIO, CONTRASEÑA);
+        return conexion;
+    }
+}
